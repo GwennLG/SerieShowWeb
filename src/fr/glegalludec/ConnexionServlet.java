df2c0b97179c8fffe2c2c9f42ccf10c8d0e03ec8
leabvr <lea.bouvier974@gmail.com>
2019-01-17 15:58:58 +0100

    mini changement

--- src/fr/glegalludec/ConnexionServlet.java
+++ src/fr/glegalludec/ConnexionServlet.java
@@ -6,18 +6,23 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.glegalludec.LoginPostName;
 
 /**
- * Servlet implementation class EspaceMembre
+ * Servlet implementation class ConnexionServlet
  */
-@WebServlet("/EspaceMembre")
-public class EspaceMembre extends HttpServlet {
+@WebServlet("/ConnexionServlet")
+public class ConnexionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private String loginErrorMessage = "";
+	
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public EspaceMembre() {
+    public ConnexionServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,9 +31,19 @@ public class EspaceMembre extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		this.getServletContext().getRequestDispatcher("/Pages/EspaceMembre/").forward(request, response);
-	}
+		
+		// Vérifier la présence d'une session
+		HttpSession session = request.getSession();
 
+		if (session.getAttribute(LoginPostName.USERNAME.getName()) != null) {
+			response.sendRedirect("/SerieShow/EspaceMembre");
+			
+			return;
+		} else {
+			// Si une session existe, on redirige sur une autre page
+			this.getServletContext().getRequestDispatcher("/Pages/Connexion/").forward(request, response);
+		}
+	}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
